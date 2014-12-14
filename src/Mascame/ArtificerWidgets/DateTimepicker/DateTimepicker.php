e95cd4a1ffaea9df6ea4690949dd599da048c792
Marc Mascarell <marcmascarell@gmail.com>
2014-12-14 13:26:02 +0100

    decouple datetime widget from theme

--- src/Mascame/ArtificerWidgets/DateTimepicker/DateTimepicker.php
+++ src/Mascame/ArtificerWidgets/DateTimepicker/DateTimepicker.php
@@ -7,6 +7,9 @@ class DateTimepicker extends AbstractWidget {
 	public function output()
 	{
 		?>
+		<link rel="stylesheet" href="<?= $this->package_assets ?>/datetimepicker/css/bootstrap-datetimepicker.min.css">
+		<script src="<?= $this->package_assets ?>/datetimepicker/js/bootstrap-datetimepicker.min.js "></script>
+
 		<script>
 			$(function () {
 				$('.datetimepicker').datetimepicker({
