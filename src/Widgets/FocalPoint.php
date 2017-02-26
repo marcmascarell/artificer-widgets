98d86ed68f3319d5bbedd5f314696c6df9439e4c
Marc Mascarell <marcmascarell@gmail.com>
2017-02-26 12:27:33 +0000

    Apply fixes from StyleCI

--- src/Widgets/FocalPoint.php
+++ src/Widgets/FocalPoint.php
@@ -1,13 +1,13 @@
 <?php namespace Mascame\Artificer\Widgets;
 
-class FocalPoint extends AbstractWidget {
-
-	public function output()
-	{
-		?>
-		<!--		<link rel="stylesheet" href="--><? //= self::$assetsPath ?><!--/widgets/image-focal-point/style.css">-->
-		<!--		<script src="--><? //= self::$assetsPath ?><!--/widgets/image-focal-point/jquery.focalpoint.js"></script>-->
+class FocalPoint extends AbstractWidget
+{
+    public function output()
+    {
+        ?>
+		<!--		<link rel="stylesheet" href="--><?php //= self::$assetsPath?><!--/widgets/image-focal-point/style.css">-->
+		<!--		<script src="--><?php //= self::$assetsPath?><!--/widgets/image-focal-point/jquery.focalpoint.js"></script>-->
 	<?php
-	}
 
+    }
 }
--- src/Widgets/Icheck.php
+++ src/Widgets/Icheck.php
@@ -1,52 +1,56 @@
-<?php namespace Mascame\Artificer\Widgets;
+<?php
 
-use Mascame\Artificer\Assets\AssetsManagerInterface;
-use Mascame\Artificer\Extension\ResourceCollector;
+namespace Mascame\Artificer\Widgets;
+
 use Mascame\Artificer\Fields\Field;
 use Mascame\Artificer\Widget\FieldWidget;
+use Mascame\Artificer\Extension\ResourceCollector;
+use Mascame\Artificer\Assets\AssetsManagerInterface;
 
-class Icheck extends FieldWidget  {
+class Icheck extends FieldWidget
+{
+    public $name = 'iCheck widget';
 
-	public $name = 'iCheck widget';
+    public $description = 'iCheck widget';
 
-	public $description = 'iCheck widget';
+    public $slug = 'icheck';
 
-	public $slug = 'icheck';
+    public $thumbnail = ''; // url
 
-	public $thumbnail = ''; // url
-
-	/**
-	 * @param AssetsManagerInterface $manager
-	 */
-	public function assets(AssetsManagerInterface $manager) {
-		$manager->add([
-			'jquery-cdn',
-			'https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/blue.css',
-			'https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js',
+    /**
+     * @param AssetsManagerInterface $manager
+     */
+    public function assets(AssetsManagerInterface $manager)
+    {
+        $manager->add([
+            'jquery-cdn',
+            'https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/blue.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js',
             $this->getAssetsPath('icheck-widget.js'),
-		]);
-	}
+        ]);
+    }
 
-	/**
-	 * Extension config is not available until boot
-	 *
-	 * @param ResourceCollector $collector
-	 * @return ResourceCollector
-	 */
-	public function resources(ResourceCollector $collector) {
-		$collector->publishes([__DIR__.'/../../resources/icheck' => $this->getAssetsPath()]);
+    /**
+     * Extension config is not available until boot.
+     *
+     * @param ResourceCollector $collector
+     * @return ResourceCollector
+     */
+    public function resources(ResourceCollector $collector)
+    {
+        $collector->publishes([__DIR__.'/../../resources/icheck' => $this->getAssetsPath()]);
 
-		return $collector;
-	}
+        return $collector;
+    }
 
-	/**
-	 * @param Field $field
-	 * @return Field
-	 */
-	public function field(Field $field) {
-		$field->addAttribute('data-widget-icheck', "true");
+    /**
+     * @param Field $field
+     * @return Field
+     */
+    public function field(Field $field)
+    {
+        $field->addAttribute('data-widget-icheck', 'true');
 
-		return $field;
-	}
-
+        return $field;
+    }
 }
